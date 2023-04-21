63aef75b8321972c9891d9efda8deb78c3d21bfd
mukhan350192 <mukhan350192@gmail.com>
2023-04-21 15:03:41 +0600

    update

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -523,7 +523,7 @@
                         },
                         success: function (response) {
                             console.log(response)
-                        }
+                        },
                         error: function (response){
                             console.log(response)
                         }
